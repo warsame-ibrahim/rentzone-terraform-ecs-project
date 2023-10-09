0c621e4a7591cf0fb30d89b42585c559f913e7fd
Warsame <Warsame.mxi@hotmail.com>
2023-10-09 17:46:12 +0100

    created Vpc file

--- variables.tf
+++ variables.tf
@@ -13,3 +13,47 @@ variable "enviroment" {
   description   = "enviroment"
   type          = string
 }
+
+#Vpc Variables
+variable "vpc_cidr" {
+description       = "vpc cidr block"
+type              = string
+}
+
+variable "Public_subnet_az1_cidr" {
+description     = "public subnet az1 cidr block"
+type            = string
+}
+
+variable "Public_subnet_az2_cidr" {
+description     = "public subnet az2 cidr block"
+type            = string
+}
+
+variable "Private_app_subnet_az1_cidr" {
+description     = "private app subnet az1 cidr block"
+type            = string
+}
+
+variable "Private_app_subnet_az2_cidr" {
+description     = "private app subnet az2 cidr block"
+type            = string
+}
+
+variable "Private_data_subnet_az1_cidr" {
+description     = "private data subnet az1 cidr block"
+type            = string
+}
+
+variable "Private_data_subnet_az2_cidr" {
+description     = "private data subnet az2 cidr block"
+type            = string
+}
+
+
+
+
+
+
+
+
