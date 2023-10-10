dc0fe05ae3da43a9162ca224ca6ace9fc72a322c
Warsame <Warsame.mxi@hotmail.com>
2023-10-10 14:59:00 +0100

    Created an rds

--- variables.tf
+++ variables.tf
@@ -49,11 +49,39 @@ variable "Private_data_subnet_az2_cidr" {
 description     = "private data subnet az2 cidr block"
 type            = string
 }
+
 #sg-variables
 variable "ssh_location" {
 description     = "Ip address that can ssh into the servers"
 type            = string
 }
+
+#Rds Variables
+variable "db_snapshot_identifier" {
+description     = "How to figure out what the snapshot is"
+type            = string
+}
+
+variable "db_instance_class" {
+description     = "The instance of the Rds"
+type            = string
+}
+
+variable "db_identifier" {
+description     = "database instance_identifier"
+type            = string
+}
+
+variable "multi_az" {
+description     = "Deployment of the az"
+type            = bool
+}
+
+
+
+
+
+
 
 
 
